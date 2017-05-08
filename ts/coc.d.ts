6ecba0ebe0e18215d50ce13816fc8ebe46e9a386
aimozg <aimozg@ya.ru>
2017-05-08 15:38:50 +0600

    typescripted engine/time.ts

--- ts/coc.d.ts
+++ ts/coc.d.ts
@@ -11,9 +11,13 @@ declare function doNext(fn:Function):void;
 declare function refreshStats():void;
 declare function showUpDown(arrow:String,direction:"up"|"down"):void;
 
+declare class Amily extends Creature {
+
+}
 declare class Creature {
 	public armor:Item;
 	public weapon:Item;
+	public pregnancyAdvance():void;
 }
 declare namespace Inventory {
 	function takeItem(oldItem:Item,returnTo:Function):void;
@@ -30,9 +34,18 @@ declare namespace PerkLib {
 declare class Player extends Creature {
 
 }
+declare class Tamani extends Creature {
 
+}
+
+declare let amily: Amily;
 declare let hyperHappy: boolean;
 declare let monster: Creature;
 declare let player: Player;
-
+declare let tamanipreg: Tamani;
+declare let time: {
+	minutes:number;
+	hours:number;
+	days:number;
+}
 
---
+++ ts/engine/time.ts
@@ -0,0 +1,40 @@
+// These functions handle changing the clock.
+// Right now there is no day counter.
+namespace Time {
+	function increment() {
+		time.minutes++;
+		if (time.minutes >= 60) {
+			time.minutes -= 60;
+			time.hours++;
+		}
+		if (time.hours >= 24) {
+			time.hours -= 24;
+			time.days++;
+		}
+	}
+
+	export function advanceMinutes(minutes: number) {
+		//if (timeAware.length > 0) { // If there's a function in timeAware
+		//	for (i = 0; i < timeAware.length; i++) {
+		//		timeAware[i].advanceTime(minutes);
+		//	}
+		for (let i = 0; i < minutes; i++) {
+			increment();
+			player.pregnancyAdvance(); // Advances the Player's pregnancy.
+			amily.pregnancyAdvance(); // Advances Amily's pregnancy.
+			tamanipreg.pregnancyAdvance(); //Advances Tamani's pregnancy.
+		}
+		//pregnancyProgression.updatePregnancy(); // Outputs the results of the Player's pregnancy flags once time passes.
+	}
+
+	export function advanceHours(hours: number) {
+		Time.advanceMinutes(hours * 60);
+	}
+}
+
+interface TimeAware {
+	advanceTime(minutes: number): void;
+}
+
+// Global array for loading in pregnancies and other things that are time sensitive.
+const timeAware: TimeAware[] = [];
